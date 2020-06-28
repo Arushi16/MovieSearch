61ae6b773c1c9fc8b36ce52f78dd1897cb15e220
Arushi16 <arushi.g151@gmail.com>
2020-06-28 11:08:09 +0530

    Updates

---
+++ precache-manifest.d8f5e90fb49723c0f1884166134e4512.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9a753be71208c37fc7f5171d3ca7e10",
+    "url": "/MovieSearch/index.html"
+  },
+  {
+    "revision": "3c9c38f6ec3566f48570",
+    "url": "/MovieSearch/static/css/main.f229a2cc.chunk.css"
+  },
+  {
+    "revision": "a88a44ea2b5d0cc3bb87",
+    "url": "/MovieSearch/static/js/2.bb417f4c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MovieSearch/static/js/2.bb417f4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c9c38f6ec3566f48570",
+    "url": "/MovieSearch/static/js/main.348c22d2.chunk.js"
+  },
+  {
+    "revision": "72264137bc8573bb0d77",
+    "url": "/MovieSearch/static/js/runtime-main.e11f3100.js"
+  },
+  {
+    "revision": "6695af9986412e75985538255ca87866",
+    "url": "/MovieSearch/static/media/404.6695af99.svg"
+  }
+]);
